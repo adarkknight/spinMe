272d3fdb5c62a9edca65071339be203ae34fa1d6
Christopher Brock <brock.christopherj@gmail.com>
2023-09-18 14:38:14 -0500

    ACT-12 Some work to get the footer working

---
+++ app/footer.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+import { Footer as DaisyFooter } from 'react-daisyui'
+const Footer = () => {
+  return (
+    <DaisyFooter className="footer footer-center bg-neutral font-inter text-neutral-content text-xs font-bold mt-auto">
+        <div>
+        <p>Copyright © 2023 - All rights reserved by Action Labs</p>
+        </div>
+    </DaisyFooter>
+  )
+}
+
+export default Footer
