e5ff8ecd11687acaf3542e7329494b04b51fd950
Christopher Brock <brock.christopherj@gmail.com>
2023-09-21 11:34:40 -0500

    ACT-15 Get things back to working state

--- app/components/recommendation.tsx
+++ app/components/recommendation.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import Image from "next/image";
 import Button from "./button";
@@ -15,39 +16,11 @@ interface AlbumInfo {
     cover_image: string;
 }
 const Recommendation = ({ onClose }: Props) => {
-    const [albumData, setAlbumData] = React.useState({
-        title: '',
-        artistName: '',
-        coverImage: '',
-    });
-
     const albumImage = require('../../public/ramones1.png');
+    const artistName = 'Ramones';
+    const albumName = 'Ramones';
 
-    React.useEffect(() => {
-        fetch(`https://api.discogs.com/users/adarkknight/collection/folders/1/releases?token=${process.env.NEXT_PUBLIC_DISCOGS_TOKEN}`, {
-            headers: {
-                'User-Agent': 'SpinMe/0.1'
-            }
-        })
-            .then((response) => response.json())
-            .then((responseData) => {
-                return responseData.releases[0].basic_information
-            })
-            .then((response: AlbumInfo) => {
-                const { title, artists, cover_image } = response;
-                const artistName = artists[0].name;
-                const coverImage = cover_image;
-                setAlbumData({
-                    title,
-                    artistName,
-                    coverImage,
-
-                })
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }, []);
+   
 
     return (
 
@@ -56,13 +29,13 @@ const Recommendation = ({ onClose }: Props) => {
             <div className="modal-container">
                 <div className="modal-content">
                     <div className="card w-50 glass">
-                        <Image src={albumData.coverImage} className="w-28 rounded-lg mx-auto mt-6" alt="album" />
+                        <Image src={albumImage} className="w-28 rounded-lg mx-auto mt-6" alt="album" />
                         <div className="card-body">
                             <div className="font-inter text-sm font-semibold justify-center" >We think you should spin</div>
                             <div className="flex-col justify-center items-center">
-                                <p className="font-inter text-sm font-light italic">{albumImage}</p>
+                                <p className="font-inter text-sm font-light italic">{albumName}</p>
                                 <p className="font-inter text-sm">by</p>
-                                <p className="font-inter font-black ">{albumData.artistName}</p>
+                                <p className="font-inter font-black ">{artistName}</p>
                             </div>
                             <div className="card-actions justify-end">
                             </div>
@@ -77,5 +50,3 @@ const Recommendation = ({ onClose }: Props) => {
 }
 
 export default Recommendation;
-
-/// wrap in div, display-flex
