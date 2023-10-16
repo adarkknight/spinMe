c74e5c453a966137ec584001274930728594923f
Christopher Brock <brock.christopherj@gmail.com>
2023-10-16 10:25:18 -0500

    ACT-25 Added loading components

---
+++ app/home/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoadingSpinner from '../components/loadingSpinner'
+
+const Loading = () => {
+  return (
+    <LoadingSpinner />
+  )
+}
+
+export default Loading
