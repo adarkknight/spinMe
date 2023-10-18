86ba543678d7bc1bfa2155237f0e47ad787fd2cc
Christopher Brock <brock.christopherj@gmail.com>
2023-10-18 11:22:34 -0500

    ACT-23 Updated button styling, removed unused imports

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -1,34 +1,36 @@
 'use client';
 import { useRouter } from 'next/navigation'
 import React from 'react'
-import {useSearchParams} from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 interface Props {
-    currentPage: number,
-    pageCount: number,
+  currentPage: number,
+  pageCount: number,
 }
 const Pagination = ({
-    pageCount, 
-    currentPage
-    }: Props) => {
-        const router = useRouter();
-        const searchParams = useSearchParams();
-        const changePage = (page: number) => {
-            const params = new URLSearchParams(searchParams)
-            params.set('page', page.toString());
-            router.push('?' + params.toString());
-        }
+  pageCount,
+  currentPage
+}: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams)
+    params.set('page', page.toString());
+    router.push('?' + params.toString());
+  }
   return (
-    <div className="join grid grid-cols-2 mb-2">
-    {currentPage === 1 &&
-    <button className="join-item btn btn-outline btn-disabled">Previous</button> ||
-    <button className="join-item btn btn-outline" onClick={() => {changePage(currentPage - 1)}}>Previous</button>
-    }
-    {currentPage >= pageCount &&
-    <button className="join-item btn btn-outline btn-disabled">Next</button> ||
-    <button className="join-item btn btn-outline" onClick={() => {changePage(currentPage + 1)}}>Next</button>
-    }
-  </div>
+    <div className='flex flex-col items-center'>
+      <div className="join grid grid-cols-2 mb-2">
+        {currentPage === 1 &&
+          <button className="join-item btn btn-outline btn-disabled">Previous</button> ||
+          <button className="join-item btn btn-outline" onClick={() => { changePage(currentPage - 1) }}>Previous</button>
+        }
+        {currentPage >= pageCount &&
+          <button className="join-item btn btn-outline btn-disabled">Next</button> ||
+          <button className="join-item btn btn-outline" onClick={() => { changePage(currentPage + 1) }}>Next</button>
+        }
+      </div>
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Navbar from './navbar'
 import Footer from './footer'
 
 const inter = Inter({ subsets: ['latin'] })
