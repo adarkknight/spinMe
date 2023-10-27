15afa2a3b63d4b78b701222b3a5255b668fa8b0d
Christopher Brock <brock.christopherj@gmail.com>
2023-10-27 10:10:32 -0500

    ACT-30 Add discogs reference, styling updates

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center bg-neutral font-inter text-neutral-content text-xs font-bold mt-auto">
+    <footer className="footer footer-center bg-neutral font-semibold text-neutral-content text-[10px] leading-3">
       <aside>
-        <p>Copyright © 2024 - spinMe</p>
+        <div>Copyright © 2024 - spinMe</div>
+        <div>Data provided by
+          <a href="https://www.discogs.com/" target='_blank' rel='noopener noreferrer' className="link link-hover"> Discogs
+          </a>
+        </div>
       </aside>
     </footer>
-        )
+  )
 }
 
 export default Footer
