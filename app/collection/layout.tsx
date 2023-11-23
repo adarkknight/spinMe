038baed097fad2893a57df8473839309f1bdc175
Christopher Brock <brock.christopherj@gmail.com>
2023-11-22 20:13:47 -0600

    ACT-32 Add conditional rendering to Collection page navbar

--- app/collection/layout.tsx
+++ app/collection/layout.tsx
@@ -1,12 +1,20 @@
 import React, { ReactNode } from 'react'
 import Navbar from '../navbar'
+import { currentUser } from '@clerk/nextjs';
+
 interface Props {
     children: ReactNode;
 }
-const CollectionLayout = ({ children }: Props) => {
+const CollectionLayout = async function ({ children }: Props) {
+    const user = await currentUser();
+    let isAuthorized = false;
+
+    if (user?.privateMetadata.discogsToken && user?.privateMetadata.discogsUserId) {
+        isAuthorized = true;
+    }
     return (
         <>
-            <Navbar />
+            <Navbar authorized={isAuthorized} />
             <div>{children}</div>
         </>
 
