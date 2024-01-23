72673d0968be4e1a8aaa0a02328ac148d646efaf
Christopher Brock <brock.christopherj@gmail.com>
2024-01-23 14:40:32 -0600

    ACT-35 Update hero to use hardcoded data when user not authorized

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,8 @@
-import Hero from '../hero'
-import getAlbumData from '../api/getAlbumData';
-import { currentUser } from '@clerk/nextjs';
-import Placeholder from './placeholder';
+import Hero from "../hero";
+import getAlbumData from "../api/getAlbumData";
+import { currentUser } from "@clerk/nextjs";
+import Placeholder from "./placeholder";
+import placeHolderData from "../data/data";
 
 export default async function Home() {
   let authorized = false;
@@ -10,23 +11,25 @@ export default async function Home() {
   let data;
   const user = await currentUser();
 
-  if (user?.privateMetadata.discogsToken && user?.privateMetadata.discogsUserId) {
+  if (
+    user?.privateMetadata.discogsToken &&
+    user?.privateMetadata.discogsUserId
+  ) {
     authorized = true;
     discogsToken = user?.privateMetadata.discogsToken as string;
     discogsUserId = user?.privateMetadata.discogsUserId as string;
     data = await getAlbumData(discogsToken, discogsUserId);
+  } else {
+    // For demo purposes, if user is not authorized with Discogs,
+    // show hardcoded data
+    data = placeHolderData;
   }
 
+  // To Do: Place holder for discogs syncing
+  // return <main>{authorized ? <Hero data={data} /> : <Placeholder />}</main>;
   return (
     <main>
-      {authorized ? (
-        <Hero data={data} />
-      ) : (
-        <Placeholder />
-      )
-
-      }
-
+      <Hero data={data} />
     </main>
-  )
+  );
 }
